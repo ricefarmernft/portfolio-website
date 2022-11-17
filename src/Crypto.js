4f68bb55a57b3c88df43614ea1e6d2d25f8b69d4
Rice <112427358+ricefarmernft@users.noreply.github.com>
2022-11-17 11:16:27 -0600

    Updated links and CSS

--- src/Crypto.js
+++ src/Crypto.js
@@ -9,9 +9,18 @@ export default function Crypto() {
           <p>
             Ceejay is an avid crypto and Web 3 supporter and is heavily involved
             in the industry. He has experience with DeFi protocols, smart
-            contracts, and NFTs. On Twitter, he is a content creator and has
-            amassed over 20k+ followers. He is also apart of numerous NFT
-            communities and enjoys marketing and community building.
+            contracts, and NFTs. On{" "}
+            <a
+              className="crypto-link"
+              href="https://twitter.com/RiceFarmerNFT"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+            , he is a content creator and has amassed over 20k+ followers. He is
+            also apart of numerous NFT communities and enjoys marketing and
+            community building.
           </p>
         </div>
         <div className="crypto-img">
@@ -21,7 +30,14 @@ export default function Crypto() {
             src="https://res.cloudinary.com/dctoezvq5/image/upload/v1668276832/portfolio/portfolio-image_b2lvt3.png"
           ></img>
           <br />
-          <a className="link" href="https://opensea.io/RiceFarmerVault" target="_blank" rel="noreferrer">NFT Collection</a>
+          <a
+            className="link"
+            href="https://opensea.io/RiceFarmerVault"
+            target="_blank"
+            rel="noreferrer"
+          >
+            NFT Collection
+          </a>
         </div>
       </div>
     </>
