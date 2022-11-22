f4d1cbe3273b0e1be64cf7a0457a671db469c28f
Rice <112427358+ricefarmernft@users.noreply.github.com>
2022-11-22 17:07:37 -0600

    Added reset form

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -10,9 +10,9 @@ export default function AboutMe() {
         <div className="about-me-text">
           <p>
             Ceejay Xiong studied Accounting at The Ohio State University. After
-            working as a financial analyst for a national bank and a
-            crypto startup, Ceejay became interested in Web Development. He left
-            his job and is currently pursuing front-end programming full-time.
+            working as a financial analyst for a national bank and a crypto
+            startup, Ceejay became interested in Web Development. He is
+            currently pursuing front-end programming full-time.
           </p>
         </div>
         <div className="about-me-text1">
@@ -21,25 +21,29 @@ export default function AboutMe() {
             enjoys reading, watching movies, and staying active.
           </p>
           <p>
-            Check out his <Link
-                className="about-me-link"
-                to="portfolio"
-                spy={true}
-                smooth={true}
-                offset={-75}
-                duration={500}
-              >
-                portfolio
-              </Link> or <Link
-                className="about-me-link"
-                to="contact"
-                spy={true}
-                smooth={true}
-                offset={0}
-                duration={1000}
-              >
-                contact
-              </Link> him for your web development needs.
+            Check out his{" "}
+            <Link
+              className="about-me-link"
+              to="portfolio"
+              spy={true}
+              smooth={true}
+              offset={-75}
+              duration={500}
+            >
+              portfolio
+            </Link>{" "}
+            or{" "}
+            <Link
+              className="about-me-link"
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={0}
+              duration={1000}
+            >
+              contact
+            </Link>{" "}
+            him for your web development needs.
           </p>
         </div>
       </div>
