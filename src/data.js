17775b6fea7f25c4ff09c8e2b2b15ef2e22a706e
Rice <112427358+ricefarmernft@users.noreply.github.com>
2022-11-27 13:31:52 -0600

    updated images

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ export const data = [
     description:
       "Opensea has a policy where you are able to receive refunds for failed gas transactions. This app filters your failed Opensea transactions into a neat table that you can forward to their help center for a reimbursement.",
     image:
-      "https://res.cloudinary.com/dctoezvq5/image/upload/c_scale,h_300,w_400/v1669155828/portfolio/openseaunsplash_rs1gv1.jpg",
+      "https://res.cloudinary.com/dctoezvq5/image/upload/v1669576048/portfolio/opensea-image_l1uuaa.png",
     alt: "Opensea Reimbursement",
     href: "https://opensea-failed-transactions.vercel.app/"
   },
@@ -15,7 +15,7 @@ export const data = [
     description:
       "As an avid movie watcher, I wanted a way to track movies that I have watched and assign a rating (0-10) to them. Movie data is received from the Open Movie Database API. Once added, movies can be sorted by rating or alphabetically.",
     image:
-      "https://res.cloudinary.com/dctoezvq5/image/upload/c_scale,h_300,w_400/v1668212994/portfolio/pexels-louis-1200450_gzkc7p.jpg",
+      "https://res.cloudinary.com/dctoezvq5/image/upload/v1669576647/portfolio/movie-tracker-image_fiud4a.png",
     alt: "Movie Theatre",
     href: "https://movie-tracker-eight.vercel.app/"
   },
@@ -25,7 +25,7 @@ export const data = [
     description:
       "An interactive family tree featuring the Targaryen family from the reign of Aegon the Conqueror to the Dance of the Dragons. Desktop only.",
     image:
-      "https://res.cloudinary.com/dctoezvq5/image/upload/c_scale,h_300,w_400/v1668222430/portfolio/targaryenfamily_cwvfle.jpg",
+      "https://res.cloudinary.com/dctoezvq5/image/upload/v1669577106/portfolio/targaryen-family-image_mrwdug.png",
     alt: "Targaryen Family",
     href: "https://targaryen-family-tree.vercel.app/"
   },
@@ -35,7 +35,7 @@ export const data = [
     description:
       "A mock crypto e-commerce website created as a part of my final React course project implementing all common React concepts. Features the ability to add items to a shopping cart and checkout via credit card.",
     image:
-      "https://res.cloudinary.com/dctoezvq5/image/upload/c_scale,h_300,w_400/v1668269061/portfolio/pexels-david-mcbee-730564_vosjqc.jpg",
+      "https://res.cloudinary.com/dctoezvq5/image/upload/v1669577115/portfolio/storefront-image_vm5lnf.png",
     alt: "Crypto E-Commerce Site",
     href: "https://crypto-ecommerce-site.vercel.app/"
   },
