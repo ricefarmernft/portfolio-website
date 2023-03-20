d20bdda05e8385f2ba200f18434b4363486dbe7b
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-03-19 21:42:27 -0500

    fixed formatting

--- src/data.js
+++ src/data.js
@@ -1,6 +1,18 @@
 export const data = [
   {
+    id: 0,
+    link: "icebreakers",
+    title: "Icebreakers",
+    description:
+      "Ever spent ages trying to come up with a clever opener for your online dating matches? Well, icebreakers is here to save the day! This nifty tool whips up pickup lines tailored to your match's name, gender, and a fun fact from their profile, so you can kick off those chats and make meaningful connections in no time.",
+    image:
+      "https://res.cloudinary.com/dctoezvq5/image/upload/v1679269462/portfolio/icebreakers/computer-icebreakers-bg_o6a1gm.png",
+    alt: "Icebreakers",
+    href: "https://icebreakers.dev/",
+  },
+  {
     id: 1,
+    link: "forgotten-apes",
     title: "Forgotten Apes",
     description:
       "How many Bored Apes are lost forever? This is my attempt at finding that number by analyzing Blockchain data and displaying it in an easy to read and simple User Interface.",
@@ -11,6 +23,7 @@ export const data = [
   },
   {
     id: 2,
+    link: "opensea-refund",
     title: "Opensea Refund",
     description:
       "Opensea has a policy where you are able to receive refunds for failed gas transactions. This app filters your failed Opensea transactions into a neat table that you can forward to their help center for a reimbursement.",
@@ -21,6 +34,7 @@ export const data = [
   },
   {
     id: 3,
+    link: "movie-rating-tracker",
     title: "Movie Rating Tracker",
     description:
       "As an avid movie watcher, I wanted a way to track movies that I have watched and assign a rating (0-10) to them. Movie data is received from the Open Movie Database API. Once added, movies can be sorted by rating or alphabetically.",
