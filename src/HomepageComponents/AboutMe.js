4a62dc98e6f9cec346a1cf0a74d95268d8ef5de6
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-03-19 21:42:05 -0500

    updated AboutMe info

--- src/HomepageComponents/AboutMe.js
+++ src/HomepageComponents/AboutMe.js
@@ -9,11 +9,18 @@ export default function AboutMe() {
       <div className="container about-me">
         <div className="about-me-text">
           <p>
-            As a web developer, I excel in crafting captivating websites using
-            React, Javascript, and CSS. In particular, my passion for Web 3
-            inspires me to build blockchain and crypto applications. When I'm
-            not programming, I enjoy playing poker, immersing myself in the
-            imaginative realm of anime, and sharing my thoughts and creations on{" "}
+            Coding has opened up a whole new world of problem-solving for me,
+            unleashing a wave of creativity I never knew I had. As a web
+            developer, I excel in crafting captivating websites using React,
+            Javascript, and CSS. In particular, my passion for Web 3 inspires me
+            to build blockchain and crypto applications.
+          </p>
+        </div>
+        <div className="about-me-text1">
+          <p>
+            When I'm not programming, I enjoy playing poker, immersing myself in
+            the imaginative realm of anime, and sharing my thoughts and
+            creations on{" "}
             <a
               className="crypto-link"
               href="https://twitter.com/RiceFarmerNFT"
@@ -23,14 +30,6 @@ export default function AboutMe() {
               Twitter
             </a>
             .
-          </p>
-        </div>
-        <div className="about-me-text1">
-          <p>
-            Coding has opened up a whole new world of problem-solving for me,
-            unleashing a wave of creativity I never knew I had. I'm eager to
-            continue growing and improving as a front-end developer in this
-            exciting field.
           </p>
           <p>
             If you're interested in my work, please feel free to check out my{" "}
