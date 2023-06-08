ee0f982689c3eea91456bad8305b0a6383f18819
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-06-08 15:32:11 -0500

    updated skills

--- src/HomepageComponents/Skills.js
+++ src/HomepageComponents/Skills.js
@@ -6,22 +6,22 @@ export default function Skills() {
       </div>
       <div className="container skills">
         <ul>
+          <li>Next.js</li>
           <li>React</li>
           <li>Javascript</li>
+          <li>PostgreSQL</li>
           <li>Node.js</li>
           <li>Ethers.js</li>
           <li>OpenAi</li>
           <li>CSS</li>
           <li>HTML</li>
           <li>Git</li>
-          <li>Photoshop</li>
-          <li>Firebase</li>
+          <li>Supabase</li>
           <li>Postman</li>
           <li>Crypto</li>
           <li>Blockchain</li>
           <li>DeFi</li>
           <li>NFTs</li>
-          <li>Social Media</li>
         </ul>
       </div>
     </>
